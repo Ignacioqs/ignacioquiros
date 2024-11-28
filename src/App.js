ae7d682471cc4fcb2b9ac2fb47b1ee464497e249
Ignacio Quiros <ignacioquiros@Ignacios-MacBook-Pro.local>
2024-11-28 15:39:59 -0600

    react router fix3

--- src/App.js
+++ src/App.js
@@ -2,19 +2,17 @@ import Header from "./components/header/Header";
 import Home from "./components/home/Home";
 import Bio from "./components/bio/Bio";
 import classes from "./assets/styles/app.module.css";
-import { Route, Routes, Navigate} from "react-router-dom";
+import { Route, Routes, Navigate } from "react-router-dom";
 
 function App() {
   return (
     <div className={classes.container}>
       <Header />
-    
       <Routes>
         <Route path="/" element={<Navigate replace to="/home" />} />
         <Route path="/home" element={<Home />} />
         <Route path="/bio" element={<Bio />} />
       </Routes>
-    
     </div>
   );
 }
